85732ddb8b29a2447acc16b043df4bf47f34d541
Kirill_N <sticheg2580@gmail.com>
2023-10-05 18:32:45 +0300

    redoc for nginx

--- backend/hackathon_projects/api/v_1/views/profile_views.py
+++ backend/hackathon_projects/api/v_1/views/profile_views.py
@@ -1,3 +1,4 @@
+from drf_spectacular.utils import extend_schema
 from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
@@ -6,6 +7,12 @@ from rest_framework.response import Response
 from api.v_1.serializers.profile_serializers import ProfileSerializer
 
 
+@extend_schema(
+    operation_id="Get User profile",
+    tags=("User Profile",),
+    description="Получение информации о пользователе.",
+    responses=ProfileSerializer,
+)
 @api_view(["GET"])
 @permission_classes((IsAuthenticated,))
 def profile_view(request):
