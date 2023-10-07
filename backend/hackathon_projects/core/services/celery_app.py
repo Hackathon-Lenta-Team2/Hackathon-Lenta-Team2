713c33ae80b162954708b7d3d346fffccf5bf711
Kirill_N <sticheg2580@gmail.com>
2023-10-07 17:45:35 +0300

    celery tasks finished

--- backend/hackathon_projects/core/services/celery_app.py
+++ backend/hackathon_projects/core/services/celery_app.py
@@ -1,6 +1,7 @@
 import os
 
 from celery import Celery
+from celery.schedules import crontab
 from django.conf import settings
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hackathon_projects.settings")
@@ -9,3 +10,11 @@ app = Celery("hackathon_projects")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.conf.broker_url = settings.CELERY_BROKER_URL
 app.autodiscover_tasks()
+
+app.conf.beat_schedule = {
+    # Executes every day at an hour specified in .env
+    "prepare_ds_data_every_day": {
+        "task": "tasks.collect_sales_data_for_ds_service",
+        "schedule": crontab(hour=settings.RUN_DS_DATA_PREPARATION_CRON_HOUR),
+    },
+}
--- backend/hackathon_projects/core/services/tasks.py
+++ backend/hackathon_projects/core/services/tasks.py
@@ -1,14 +1,28 @@
-import time
+from celery import shared_task
+from loguru import logger
 
-from core.services.celery_app import app
+from core.utils.csv_writer import CSVWriter
+from core.utils.json_data_import import import_data_from_json
+from core.utils.requests import send_signal_to_ds_service
 
 
-@app.task()
-def debug_task(string: str):
-    time.sleep(10)
-    print(f"Hello from debug_task. {string=}")
-    print(f"Hello from debug_task. {string=}")
-    print(f"Hello from debug_task. {string=}")
-    print(f"Hello from debug_task. {string=}")
+@shared_task(name="tasks.ds_data_import_task")
+def ds_data_import_task() -> None:
+    """Запускает импорт данных от DS сервиса в БД с помощью Celery."""
+    try:
+        import_data_from_json("forecast_archive.json")
+        logger.info("Импорт завершён.")
+    except Exception as err:
+        error_message = f"Unexpected {err=}"
+        logger.error(error_message)
 
-    # вызывать так debug_task.delay("Бла бла")
+
+@shared_task(name="tasks.collect_sales_data_for_ds_service")
+def collect_sales_data_for_ds_service() -> None:
+    """Собирает данные фактических продаж.
+    Пишет данные в ds_data.csv и уведомляет ds сервис.
+    """
+    # записать файл данных
+    CSVWriter().write()
+    # послать сигнал ds сервису
+    send_signal_to_ds_service()
