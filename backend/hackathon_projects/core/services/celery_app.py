4c31a707a1bf13167b9ca8dcfb59da0a8b049bfa
Kirill_N <sticheg2580@gmail.com>
2023-10-07 22:31:43 +0300

    final config

--- backend/hackathon_projects/core/services/celery_app.py
+++ backend/hackathon_projects/core/services/celery_app.py
@@ -12,7 +12,8 @@ app.conf.broker_url = settings.CELERY_BROKER_URL
 app.autodiscover_tasks()
 
 app.conf.beat_schedule = {
-    # Executes every day at an hour specified in .env
+    # Executes every day at an hour specified in .env.
+    # Env var - RUN_DS_DATA_PREPARATION_CRON_HOUR.
     "prepare_ds_data_every_day": {
         "task": "tasks.collect_sales_data_for_ds_service",
         "schedule": crontab(hour=settings.RUN_DS_DATA_PREPARATION_CRON_HOUR),
