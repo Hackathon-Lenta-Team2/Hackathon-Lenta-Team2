9db49fe3bfc6b2e09328467bcef5445ee6eee3fe
Kirill_N <sticheg2580@gmail.com>
2023-10-07 19:32:57 +0300

    ds login config

--- ds/scripts/src/app.py
+++ ds/scripts/src/app.py
@@ -1,46 +1,74 @@
 import argparse
-import datetime
-import logging
 import json
-import uvicorn
-from fastapi import FastAPI, BackgroundTasks
-from model import forecast
+import logging
+import os
+
 import requests
+import uvicorn
+from fastapi import BackgroundTasks, FastAPI
+
+from model import forecast
 
 app = FastAPI()
 
-dataDir = 'tmp/'
+dataDir = "tmp/"
+
+CREDENTIALS = {
+    "email": os.getenv("DS_SERVICE_LOGIN", default="ds@mail.ru"),
+    "password": os.getenv("DS_SERVICE_PASSWORD", default="ds_password"),
+}
 
 app_logger = logging.getLogger(__name__)
 app_logger.setLevel(logging.DEBUG)
 app_handler = logging.StreamHandler()
-app_formatter = logging.Formatter("%(name)s %(asctime)s %(levelname)s %(message)s")
+app_formatter = logging.Formatter(
+    "%(name)s %(asctime)s %(levelname)s %(message)s"
+)
 app_handler.setFormatter(app_formatter)
 app_logger.addHandler(app_handler)
 
 
-def make_forecast(path: str) -> tuple:
-    """runs forecast and save result"""
-    app_logger.info(f'data successfully loaded')
+def send_signal_to_back():
+    """Logs in and sends signal to backend."""
+    try:
+        login_resp = requests.post(
+            "http://localhost/auth/token/login/", data=CREDENTIALS
+        )
+        token = login_resp.json().get("auth_token")
+        if not token:
+            app_logger.info("Authorization FAILED.")
+            return
+        app_logger.info("Authorization succeeded.")
+
+        headers = {"Authorization": f"Token {token}"}
+        signal_resp = requests.get(
+            "http://localhost/api/v1/import-forecasts/", headers=headers
+        )
+        if signal_resp.status_code == 200:
+            app_logger.info("Backend was notified.")
+            return
+        app_logger.info("Backend was not notified.")
+
+    except (requests.exceptions.ConnectionError, Exception) as er:
+        app_logger.info("Backend is not available.")
+
+
+def make_forecast(path: str) -> None:
+    """Runs forecast and saves result."""
+    app_logger.info(f"data successfully loaded")
     result, status, problem_pairs = forecast(path)
-    message = 'forecast successfully finished, results saved'
-    if status != 'OK':
-        app_logger.error(f'forecast failed')
-        message = 'forecast failed'
+    message = "forecast successfully finished, results saved"
+    if status != "OK":
+        app_logger.error(f"forecast failed")
+        message = "forecast failed"
     else:
-        app_logger.info(f'forecast finished')
-        with open(dataDir + 'forecast_archive.json', 'w') as file:
+        app_logger.info(f"forecast finished")
+        with open(dataDir + "forecast_archive.json", "w") as file:
             json.dump(result, file)
-            app_logger.info(f'data saved')
+            app_logger.info(f"data saved")
     app_logger.info(message)
     # resp = requests.get("http://localhost:8001/ds/ready")  # for local tests
-    resp = requests.get("http://localhost/api/v1/import-data/")
-    result = {'date': datetime.datetime.now(), 'status': message, 'problem pairs number': problem_pairs}
-    if resp.status_code != 200:
-        result = 'something wrong'
-    print(result)
-    print(resp.status_code)
-    return result, resp.status_code
+    send_signal_to_back()
 
 
 # for local tests
@@ -51,8 +79,8 @@ def make_forecast(path: str) -> tuple:
 
 @app.get("/ds/start")
 async def main(background_tasks: BackgroundTasks) -> dict:
-    """running forecast in background"""
-    background_tasks.add_task(make_forecast, path=dataDir + 'ds_data.csv')
+    """Runs forecast in the background."""
+    background_tasks.add_task(make_forecast, path=dataDir + "ds_data.csv")
     return {"message": "forecast is running. wait please"}
 
 
