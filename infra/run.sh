9308e025f4e8982f4e4703fcaa4069782be0248f
Kirill_N <sticheg2580@gmail.com>
2023-10-08 13:58:45 +0300

    more user friendly scripts

--- infra/run.sh
+++ infra/run.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+echo  "Launching the service..."
+
 cat .env.example > .env
 
 cat .env.example_ds > .env.ds
 
 docker-compose up -d
+
+echo  "Service started successfully."
+
+sleep 2
