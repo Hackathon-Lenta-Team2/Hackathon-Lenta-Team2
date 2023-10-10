bde75c814b41dfdabd0241dba4a2de3c745d0387
Kirill_N <sticheg2580@gmail.com>
2023-10-10 15:14:09 +0300

    removed comment

--- backend/hackathon_projects/api/v_1/serializers/forecasts_serializers.py
+++ backend/hackathon_projects/api/v_1/serializers/forecasts_serializers.py
@@ -21,9 +21,3 @@ class ForecastSerializer(ModelSerializer):
     class Meta:
         model = Forecast
         fields = ["store", "sku", "forecast_date", "forecast_data"]
-
-    # def get_forecast_data(self, obj):
-    #     forecast_data_date = self.context.get('forecast_data_date')
-    #     filtered_data = {date: value for date, value in obj.forecast_data.data.items(
-    #     ) if date == forecast_data_date}
-    #     return [{'data': filtered_data}]
